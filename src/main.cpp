f489a76e926dac5de75a0fc20cc3f321d4ba0e05
Pardal <marcosnascimento@eng.ci.ufpb.br>
2024-10-22 20:45:28 -0300

    adding round robin to main function

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "FileReader.h"
 #include "FCFS.h"
 #include "SJF.h"
+#include "RoundRobin.h"
 
 using namespace std;
 
@@ -16,6 +17,7 @@ int main() {
 
   queues.push_back(make_unique<FCFS>(file_reader.get_processes()));
   queues.push_back(make_unique<SJF>(file_reader.get_processes()));
+  queues.push_back(make_unique<RoundRobin>(file_reader.get_processes(), 2));
 
   for (const auto& queue : queues) {
     queue->execute();
